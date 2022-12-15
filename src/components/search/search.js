df7483169301912b83a37f0a4823070308811df9
sean-code <john.nganga@student.moringaschool.com>
2022-12-15 23:39:33 +0300

    Create and style the search form

---
+++ src/components/search/search.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./search.css"
+
+
+function Search(){
+    return(
+        <div className="wrap">
+            <div className="search">
+                <input type="text" className="searchTerm" placeholder="What are you looking for?" />
+                <button type="submit" className="searchButton">
+                <i className="fa fa-search"></i>
+                </button>
+            </div>
+        </div>
+    )
+}
+
+export default Search;
