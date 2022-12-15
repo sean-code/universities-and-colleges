1080af4e4841e507bfb798fdd57a319bb48ed15f
sean-code <john.nganga@student.moringaschool.com>
2022-12-15 23:04:53 +0300

    Import and add BrowserRouter, Routes, and Route paths with their elements

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import './App.css';
+import React from 'react';
+import {BrowserRouter, Routes, Route}  from 'react-router-dom';
+import NavBar from './components/navbar/navbar';
+import Home from './components/home/home';
+import Search from './components/search/search';
 
 function App() {
   return (
     <div>
+      <BrowserRouter>
+        <NavBar />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/search' element={< Search/>} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
